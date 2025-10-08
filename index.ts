fd0148d8d6a10432f22fa4a03dd768af6783fbb1
kunnikarB <kunnikarboon@gmail.com>
2025-10-08 14:58:23 +0200

    Create Express server with Get/userlanguages route

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
+
 app.use(express.json());
 
-
+/**
+ * GET all users
+ * GET /userlanguages
+ */
+app.get('/userlanguages', async (req, res) => {
+  try {
+    const users = await prisma.userLanguage.findMany();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 app.listen(5500, () => {
   console.log("🚀 Server is running on port 5500");
